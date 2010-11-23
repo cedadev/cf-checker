512918c36c7f4feea2cf0c44d11bc8e7bd61aaee
pascoe <pascoe@4b70ecba-f619-0410-a16a-f37793ed1c54>
2010-11-23 11:17:05 +0000

    Merging branch distutils into trunk.
    git-svn-id: http://cf-pcmdi.llnl.gov/svn/repository-cf/cf-checker/trunk@145 4b70ecba-f619-0410-a16a-f37793ed1c54

---
+++ setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+import sys, os
+
+__version__ = '2.0.2'
+__description__ = "\n"+open(os.path.join(os.path.dirname(__file__), 'src/README')).read()
+
+setup(name='cfchecker',
+      version=__version__,
+      description="The NetCDF Climate Forcast Conventions compliance checker",
+      long_description=__description__,
+      
+      classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Scientific/Engineering :: Atmospheric Science',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        ], 
+
+      keywords='',
+      author='Rosalyn Hatcher',
+      author_email='r.s.hatcher@reading.ac.uk',
+
+      #!TODO: Maintainer becomes author in PKG-INFO so disable this for now
+      #       This is a known Python issue that may be fixed in the future.
+      #maintainer='Stephen Pascoe',
+      #maintainer_email='Stephen.Pascoe@stfc.ac.uk',
+
+      url='http://cf-pcmdi.llnl.gov/conformance/compliance-checker/',
+      package_dir = {'': 'src'},
+      packages=find_packages('src'),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        # -*- Extra requirements: -*-
+        #!NOTE: although CDAT is required we may want to use full CDAT or
+        #       cdat_lite.  Therefore don't use dependency management.
+      ],
+      entry_points= {
+        'console_scripts': ['cfchecks = cfchecker:cfchecks_main'],
+        },
+      test_suite='nose.collector',
+      )
---
+++ src/cfchecker/__init__.py
@@ -0,0 +1,15 @@
+import sys
+from cfchecker.cfchecks import getargs, CFChecker
+
+def cfchecks_main():
+    """cfchecks_main is based on the main program block in cfchecks.py
+    """
+
+    (badc,coards,uploader,useFileName,standardName,areaTypes,udunitsDat,version,files)=getargs(sys.argv)
+    
+    inst = CFChecker(uploader=uploader, useFileName=useFileName, badc=badc, coards=coards, cfStandardNamesXML=standardName, cfAreaTypesXML=areaTypes, udunitsDat=udunitsDat, version=version)
+    for file in files:
+        rc = inst.checker(file)
+        sys.exit (rc)
+
+
