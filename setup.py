080992e173af8255056cd85b838143a9437c09d6
Ag Stephens <ag.stephens@stfc.ac.uk>
2017-01-17 14:52:17 +0000

    setup.py fix
    A simple fix to the `setup.py` file: points to existing `README.md` instead of non-existent `README` file.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sys, os
 
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__))+'/src/')
 __version__ = __import__('cfchecker').__version__
-__description__ = "\n"+open(os.path.join(os.path.dirname(__file__), 'README')).read()
+__description__ = "\n"+open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
 
 setup(name='cfchecker',
       version=__version__,
