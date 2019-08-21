0dfc6b1fcf4dc2ceca38b9fdb80ddbe95e5ab5f6
RosalynHatcher <r.s.hatcher@reading.ac.uk>
2019-08-21 08:49:35 +0100

    Remove cf-checker script from setup.py

--- setup.py
+++ setup.py
@@ -41,5 +41,4 @@ setup(name='cfchecker',
       zip_safe=False,
       install_requires=['netCDF4', 'numpy>=1.7', 'cfunits>=3.0.0', 'future'],
       entry_points={'console_scripts': ['cfchecks = cfchecker.cfchecks:main']},
-      scripts=['src/cf-checker']
       )
